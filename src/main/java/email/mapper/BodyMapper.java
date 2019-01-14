e55361bd8dcade5c92ae67dce81115bec1e8a3e6
StevenMassaro <massarosareloud@aol.com>
2019-01-14 16:11:37 -0500

    refactor annotation sql statements to xml sql statements

--- src/main/java/email/mapper/BodyMapper.java
+++ src/main/java/email/mapper/BodyMapper.java
@@ -12,10 +12,8 @@ import java.util.List;
 @Mapper
 public interface BodyMapper {
 
-    @Select("INSERT INTO email.body (body) VALUES(#{body}) RETURNING id")
     long insert(@Param("body") String body);
 
-    @Select("SELECT body FROM email.body WHERE id = #{bodyId}")
     String get(@Param("bodyId") long bodyId);
 
     long delete(@Param("messages") List<Message> bodies);
