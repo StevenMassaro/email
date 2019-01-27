85488ff7c2c1d1b435bbe039edd3b80183078287
Steven Massaro <steven.massaro.web@gmail.com>
2019-01-27 09:31:43 -0500

    switch UI to react create production maven profile update gitignore

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import TableComponent from './TableComponent.js';
+
+class App extends Component {
+    render() {
+        return (
+            <TableComponent/>
+        );
+    }
+}
+
+export default App;
