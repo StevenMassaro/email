fcb3becdd5f2818abcb202e235607f62e15e46da
Steven Massaro <steven.massaro.web@gmail.com>
2020-08-31 16:44:52 -0400

    rename jenkinsfile to groovy file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+node {
+    def mvnHome
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), disableConcurrentBuilds()])
+    stage('Preparation') {
+        sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} started" || true'
+        mvnHome = tool 'M3'
+    }
+    stage('Build') {
+        // Run the maven build
+        withEnv(["MVN_HOME=$mvnHome"]) {
+            if (isUnix()) {
+                sh '"$MVN_HOME/bin/mvn" clean install -P prod,ui'
+            } else {
+                bat(/"%MVN_HOME%\bin\mvn" clean install -P prod,ui/)
+            }
+        }
+    }
+    stage('Results') {
+        //junit '**/target/surefire-reports/TEST-*.xml'
+        archiveArtifacts 'target/*.war'
+        sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} finished" || true'
+    }
+}
