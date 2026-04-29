366ddc701d5edd306abefc0c6770cd3e2ad2b9c8
Steven Massaro <steven.massaro.web@gmail.com>
2026-04-29 16:06:23 -0600

    fix dropdown closing when download is in progress

--- src/frontend/src/ModalHeaderComponent.js
+++ src/frontend/src/ModalHeaderComponent.js
@@ -5,11 +5,15 @@ import {Dropdown, Menu} from "semantic-ui-react";
 import * as lodash from "lodash";
 
 class ModalHeaderComponent extends Component {
-    downloadingRef = false;
     state = {downloadingAttachmentId: null, dropdownOpen: false};
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.downloadingAttachmentId && !this.state.dropdownOpen) {
+            this.setState({dropdownOpen: true});
+        }
+    }
+
     fetchAttachment = (attachment) => {
-        this.downloadingRef = true;
         this.setState({downloadingAttachmentId: attachment.id, dropdownOpen: true});
         fetch("./attachment?id=" + attachment.id)
             .then(function (resp) {
@@ -17,7 +21,6 @@ class ModalHeaderComponent extends Component {
             }).then((blob) => {
                 download(blob, attachment.name, attachment.contentType);
             }).finally(() => {
-                this.downloadingRef = false;
                 this.setState({downloadingAttachmentId: null, dropdownOpen: false});
             });
     };
@@ -48,7 +51,7 @@ class ModalHeaderComponent extends Component {
             <Menu>
                 <Menu.Item onClick={() => this.props.closeReadModal(currentEmail)}>Close</Menu.Item>
                 <Menu.Item onClick={() => this.print(currentEmail)}>Print</Menu.Item>
-                {!lodash.isEmpty(attachments) && <Dropdown item text='Attachments' open={dropdownOpen} closeOnChange={false} onOpen={() => this.setState({dropdownOpen: true})} onClose={() => { if (!this.downloadingRef) this.setState({dropdownOpen: false}) }}>
+                {!lodash.isEmpty(attachments) && <Dropdown item text='Attachments' open={dropdownOpen} closeOnChange={false} onOpen={() => this.setState({dropdownOpen: true})} onClose={() => this.setState({dropdownOpen: false})}>
                     <Dropdown.Menu>
                         {attachments.map((attachment) => {
                             const isDownloading = downloadingAttachmentId === attachment.id;
